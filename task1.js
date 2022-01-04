4f31cd9aa7f15e5d2dd3d01e42d1ed4dacf3d8ce
sheniaks <sheniaks@gmail.com>
2022-01-04 23:53:06 +0200

    task3

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 function calculatePaintCans(L, W, H) {
   if (L > 0 && L < 1000 && W > 0 && W < 1000 && H > 0 && H < 1000) {
     console.log(`The square is ${L * W * H} metres.`);
-    console.log(`${Math.ceil((L * W * H) / 16)} cans required.`);
+    console.log(`${Math.ceil(((L + W) * H) * 2  / 16)} cans required.`);
   } else
     console.log(
       "L,W,H should be in range from 0 to 1000. Please check function params."
---
+++ task3.js
@@ -0,0 +1,37 @@
+function correctTicketNumber(N) {
+  let str = String(N);
+  if (str.length == 5) {
+    str = "0" + str;
+  }
+  if (str.length == 4) {
+    str = "00" + str;
+  }
+  if (str.length == 3) {
+    str = "000" + str;
+  }
+  if (str.length == 2) {
+    str = "0000" + str;
+  }
+  if (str.length == 1) {
+    str = "00000" + str;
+  }
+  return str;
+}
+
+function luckyTicket(N) {
+  let ourTicket = correctTicketNumber(N);
+  let sumFirstThree =
+    Number(ourTicket[0]) + Number(ourTicket[1]) + Number(ourTicket[2]);
+  let sumLastThree =
+    Number(ourTicket[3]) + Number(ourTicket[4]) + Number(ourTicket[5]);
+  if (N > 0 && N < 10 ** 6) {
+    if (sumFirstThree == sumLastThree) {
+      console.log(`${ourTicket} - YES`);
+    } else console.log(`${ourTicket} - NO`);
+  } else console.log(`Ticket number is incorrect`);
+}
+
+luckyTicket(1);
+luckyTicket(8224);
+luckyTicket(111111);
+luckyTicket(134404);
