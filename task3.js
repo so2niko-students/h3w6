7535be155e225bc56ca04c05d10bed34e42d8190
sheniaks <sheniaks@gmail.com>
2022-01-10 11:05:18 +0200

    task 1\2

--- task3.js
+++
@@ -1,9 +0,0 @@
-function returnShortest(str) {
-  let arr = str.split(" ");
-  arr.sort(function (a, b) {
-    return a.length - b.length;
-  });
-  console.log(`${str} --> ${arr[0].length}`);
-}
-
-returnShortest("test req hello man qq re tt");
---
+++ task_20220106.js
@@ -0,0 +1,51 @@
+//TASK 1
+const ipsBetween = function (startIp, lastIp) {
+  let startArr = startIp.split(".");
+  let lastArr = lastIp.split(".");
+  let result = 0;
+  for (let i = 0; i < 4; i++) {
+    result += (lastArr[i] - startArr[i]) * 256 ** (3 - i);
+  }
+  return result;
+};
+// console.log(ipsBetween("10.0.0.0", "10.0.0.50"));
+// console.log(ipsBetween("10.0.0.0", "10.0.1.0"));
+// console.log(ipsBetween("20.0.0.10", "20.0.1.0"));
+
+//TASK 2
+const MORSE = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+    '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!',
+    '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':',
+    '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
+    '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'
+  };
+const decodeMorse = function(input){
+    function decodeMorseLetter(phrase) {
+    console.log(phrase);
+      return MORSE[phrase];
+    }
+    function decodeMorseWord(word) {
+        console.log(word);
+      return word.split(' ').map(decodeMorseLetter).join('');
+    }
+    return input.trim().split('   ').map(decodeMorseWord).join(' ');
+  }
+//  console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
+
+//TASK 3
+
+//TASK 4
+
+//TASK 5
+
+//TASK 6
+
+//TASK 7
+
