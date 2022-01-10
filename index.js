c04cb43fadb608a64d014c417f8c686a15c00920
Mike <mike.makogon@gmail.com>
2022-01-10 14:35:46 +0200

    initial submission

--- index.js
+++ index.js
@@ -1,54 +1,141 @@
 "use strict";
 
 // Task 1
-const calcSumOfMins = (arr) => {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let min = arr[i][0];
-    const innerArrLength = arr[i].length;
-    for (let j = 1; j < innerArrLength; j++) {
-      if (arr[i][j] < min) {
-        min = arr[i][j];
-      }
-    }
-    sum += min;
+const ipsBetween = (ip1, ip2) => {
+  let count = 0;
+  const aIp1 = ip1.split(".");
+  const aIp2 = ip2.split(".");
+  for (let i = 0; i < 4; i++) {
+    count += (aIp1[i] - aIp2[i]) * 256 ** (3 - i);
   }
-  return sum;
+  return count;
 };
 
 // Task 2
-const findTwoMaxValues = (arr) => {
-  if (arr.length < 2) {
-    return `Incorrect input values`;
-  }
-  arr.sort((a, b) => b - a);
-  return [arr[0], arr[1]];
+const MORSE_CODE = {
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
+  "|": " ",
+};
+
+const decodeMorse = (data) => {
+  const words = data.split(" ");
+  return words
+    .map((word) => {
+      return word
+        .split()
+        .map((letter) => MORSE_CODE[letter])
+        .join(" ");
+    })
+    .join("");
 };
 
 // Task 3
-const findShortestWord = (str) => {
-  const wordsArr = str.split(" ");
-  let shortestWordLength = wordsArr[0].length;
-  for (let i = 1; i < wordsArr.length; i++) {
-    if (wordsArr[i].length < shortestWordLength) {
-      shortestWordLength = wordsArr[i].length;
+function isSolved(board) {
+  function checkHor() {
+    let res;
+    board.forEach(function (item) {
+      if (
+        item[0] == item[1] &&
+        item[0] == item[2] &&
+        item[1] == item[2] &&
+        item[0] != 0
+      )
+        res = item[0];
+    });
+    return res;
+  }
+
+  function checkVer() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[0][i] == board[1][i] &&
+        board[1][i] == board[2][i] &&
+        board[2][i] == board[0][i] &&
+        board[0][i] != 0
+      )
+        return board[0][i];
     }
   }
-  return shortestWordLength;
-};
 
-// Task 4
-const sortNumberedWords = (str) => {
-  const wordsArr = str.split(" ");
-  const n = wordsArr.length;
-  const sortedArr = [...Array(n)];
-  for (let j = 0; j < n; j++) {
-    for (let i = 0; i < n; i++) {
-      if (wordsArr[i].includes(j + 1)) {
-        sortedArr[j] = wordsArr[i];
-        break;
+  function checkA1() {
+    if (
+      (board[0][0] == board[1][1] &&
+        board[1][1] == board[2][2] &&
+        board[2][2] == board[0][0]) ||
+      (board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0] &&
+        board[2][0] == board[0][2])
+    )
+      return board[1][1];
+  }
+
+  function gameOver() {
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++) if (board[i][j] == 0) return -1;
+    return 0;
+  }
+
+  return checkHor() || checkVer() || checkA1() || gameOver();
+}
+
+// Task 5
+const tickets = (peopleInLine) => {
+  let a25 = 0;
+  let a50 = 0;
+  for (let i = 0; i < peopleInLine.length; i++) {
+    if (peopleInLine[i] === 25) {
+      a25 += 1;
+    }
+    if (peopleInLine[i] === 50) {
+      a25 -= 1;
+      a50 += 1;
+    }
+    if (peopleInLine[i] === 100) {
+      if (a50 === 0 && a25 >= 3) {
+        a25 -= 3;
+      } else {
+        a25 -= 1;
+        a50 -= 1;
       }
     }
+    if (a25 < 0 || a50 < 0) {
+      return "NO";
+    }
   }
-  return sortedArr.join(" ");
+  return "YES";
 };
