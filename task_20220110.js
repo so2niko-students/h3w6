4eec292e55dbad1156d690ce3cb3669f29340dee
sheniaks <sheniaks@gmail.com>
2022-01-11 14:02:53 +0200

    task_20220110

--- task_20220110.js
+++ task_20220110.js
@@ -1,31 +1,91 @@
-//TASK 1 
+//TASK 1
 //SUBTASK 1
-  const textNumber = function (num) {
-    const dict = [
-      ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', '', 'две', '', 'четыр', 'пят', 'шест', 'сем', 'восем', 'девят'], 
-      ['', '', 'двадцать', 'тридцать', 'сорок', '', '', '', '', 'девяносто']
-    ]; 
-    if (num >= 0 && num <= 10)
-      return dict[0][num]; 
-    if (num > 10 && num <= 19)
-      return (dict[0][num] || dict[0][num - 10]) + 'надцать'; 
-    if (num >= 20 && num <= 99) {
-      const [units=0, tens] = [...('' + num)].reverse().map(Number); 
-      return [
-        tens ? dict[1][tens] || (dict[0][tens] + 'десят') : '', 
-        units ? dict[0][units] : ''
-      ].join(' '); 
-    }
+const textNumber = function (num) {
+  const words = [
+    [
+      "ноль",
+      "один",
+      "два",
+      "три",
+      "четыре",
+      "пять",
+      "шесть",
+      "семь",
+      "восемь",
+      "девять",
+      "десять",
+      "",
+      "две",
+      "",
+      "четыр",
+      "пят",
+      "шест",
+      "сем",
+      "восем",
+      "девят",
+    ],
+    ["", "", "двадцать", "тридцать", "сорок", "", "", "", "", "девяносто"],
+  ];
+  if (num >= 0 && num <= 10) return words[0][num];
+  if (num > 10 && num <= 19)
+    return (words[0][num] || words[0][num - 10]) + "надцать";
+  if (num >= 20 && num <= 99) {
+    const [units = 0, tens] = [...("" + num)].reverse().map(Number);
+    return [
+      tens ? words[1][tens] || words[0][tens] + "десят" : "",
+      units ? words[0][units] : "",
+    ].join(" ");
   }
-  console.log(Array.from({ length: 100 }, 
-    (v, i) => i + ': ' + textNumber(i)
-  ).join('\n')); 
-
+};
+//console.log(textNumber(83));
+//Array.from({ length: 100 }, (_v, i) => i + ": " + textNumber(i)).join("\n")
 
 //SUBTASK 2
-const getStrStats = function(str) {
-    return `${str.match(/[a-z]/gi).join('').length} letters, ${str.match(/\d/g).join('').length} digits, ${str.match(/[^\w]/g).join('').length} etc symbols`;
-}
+const getStrStats = function (str) {
+  return `${str.match(/[a-z]/gi).join("").length} letters, ${
+    str.match(/\d/g).join("").length
+  } digits, ${str.match(/[^\w]/g).join("").length} etc symbols`;
+};
 // console.log(getStrStats('test12345$$#@!@!@@@'));
-//SUBTASK 3
 
+//SUBTASK 3
+const replaceStr = function (str) {
+  let result = "";
+  for (let char of str)
+    result +=
+      char === char.toUpperCase()
+        ? char.toLowerCase().replace(/[0-9]/g, "_")
+        : char.toUpperCase().replace(/[0-9]/g, "_");
+  return result;
+};
+//console.log(replaceStr(`dDDDdaaaaddDDd11`));
+
+//TASK 2
+// const circles = document.querySelectorAll(".circle");
+// const actionBtn = document.querySelector(".button");
+// let activeLight = 1;
+// actionBtn.addEventListener("click", function changeLight() {
+//   circles[activeLight].className = 'circle';
+//   activeLight++;
+//   if(activeLight > 2) {
+//    activeLight =0;
+//   }
+//   const currentLight = circles[activeLight];
+//   currentLight.classList.add(currentLight.getAttribute("color"));
+// });
+
+//TASK 3
+//SUBTASK 1
+const arr = ["Яблоко", "Банан", "Ананас"];
+console.log(arr.map((el) => el[0]));
+console.log(arr.reduce((acc, val) => acc + val[0], "").split(""));
+//SUBTASK 2
+console.log(arr.filter((el) => el[0].toLowerCase() === "а"));
+console.log(
+  arr
+    .reduce((acc, val) => (val[0].toLowerCase() === "а" ? acc + val : acc), "")
+    .split(" ")
+);
+//SUBTASK 3
+console.log(arr.forEach((el, i, arr) => (arr[i] = `${i + 1}: ${el};`)));
+console.log(arr.reduce((acc, val, i) => acc + ` ${val}`, "").split(" "));
