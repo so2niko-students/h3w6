a15f6261d2205a6d94507db5e007f2a9802a9932
sheniaks <sheniaks@gmail.com>
2022-01-12 22:15:18 +0200

    task 3 done?

--- task_20220111.js
+++ task_20220111.js
@@ -43,7 +43,50 @@ class Dictionary {
 }
 let d = new Dictionary();
 d.newEntry("Apple", "A fruit that grows on trees");
-console.log(d.look("Apple"));
-console.log(d.look("Banana"));
+// console.log(d.look("Apple"));
+// console.log(d.look("Banana"));
 
 //TASK3
+class PaginationHelper {
+  constructor(arr, int) {
+    this.arr = arr;
+    this.int = int;
+  }
+  pageCount() {
+    return Math.ceil(this.arr.length / this.int);
+  }
+  itemCount() {
+    return this.arr.length;
+  }
+  pageItemCount(page) {
+    let currentPage = this.arr.length - this.int * page; 
+    if (currentPage > 0 && page >= 0) {
+      return currentPage > this.int ? this.int : currentPage;
+    }
+    //  return this.arr.length - (pagesTotal*this.int)}
+    //  else if (this.arr.length - (pagesTotal*this.int) > this.int) {
+    //      return this.int;}
+    else return -1;
+  }
+
+  pageIndex(index) {
+      if (index>this.arr.length || index <0){
+        return -1;
+      }
+      else return Math.floor(index/ this.int);
+  }
+}
+
+const helper = new PaginationHelper(["a", "b", "c", "d", "e", "f"], 4);
+console.log(helper.pageCount()); //should == 2
+console.log(helper.itemCount()); //should == 6
+console.log(helper.pageItemCount(-1)); // should == -1 since the page is invalid
+console.log(helper.pageItemCount(0)); //should == 4
+console.log(helper.pageItemCount(1)); // last page - should == 2
+console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+// // pageIndex takes an item index and returns the page that it belongs on
+console.log(helper.pageIndex(5)); //should == 1 (zero based index)
+console.log(helper.pageIndex(2)); //should == 0
+console.log(helper.pageIndex(0)); //should == 0
+console.log(helper.pageIndex(20)); //should == -1
+console.log(helper.pageIndex(-10)); //should == -1
