e4339ef4a92bbac6fe65a551f516fa37ab35f92f
Ivan Marchuk <62245422+skinnyjazz@users.noreply.github.com>
2022-01-12 17:35:48 +0200

    homeWork 11.01.22

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
-const myError = 'You entered incorrect data!';
+class Hex {
+    constructor(num) {
+        this.num = num;
+        this.hexadecimal = num.toString(16)
+    }
 
-Array.prototype.getMinValue = function() {
-    return this.sort((a, b) => a - b)[0]
+    static parse(hex) {
+        return parseInt(hex, 16)
+    }
+
+    toString() {
+        return `0x${this.hexadecimal.toUpperCase()}`
+    }
+
+    valueOf() {
+        return this.num
+    }
+
+    toJSON() {
+        return this.toString()
+    }
+
+    plus(num) {
+        return new Hex(this.valueOf() + num.valueOf())
+    }
+
+    minus(num) {
+        return new Hex(this.valueOf() - num.valueOf())
+    }
+
 }
 
 
-let sumOfMinValue = function(arr) {
-    if (arr.length == 0 || arr.flat().some(item => item < 0)) return new Error(myError);
-    return arr.reduce((total, item) => total += item.getMinValue(), 0);
+class PaginationHelper {
+    constructor(arrayOfitem, itemOnPage) {
+        this.arrayOfitem = arrayOfitem
+        this.itemOnPage = Math.ceil(itemOnPage);
+    }
+
+    pageCount() {
+        return Math.ceil(this.lengthOfArr / this.itemOnPage)
+    }
+
+    itemCount() {
+        return this.lengthOfArr
+    }
+    pageItemCount(page) {
+        let isThatLastPage = this.lengthOfArr - this.itemOnPage * (page);
+        if (isThatLastPage < 0) { return -1 }
+        return isThatLastPage > this.itemOnPage ? this.itemOnPage : isThatLastPage
+    }
+
+    pageIndex(itemIndex) {
+        itemIndex++
+        if (itemIndex < 0 || itemIndex > this.lengthOfArr) return -1
+        return Math.ceil(itemIndex / this.itemOnPage) - 1
+    }
+
+    get lengthOfArr() {
+
+        return this.arrayOfitem.length;
+    }
 }
 
-// let sumOfMinValue = (arr) => arr.reduce((total, item) => total += item.getMinValue(), 0)
 
+class Dictionary {
+    newEntry(key, value) {
+      this[key] = value;
+    }
+    look(key) {
+      return this[key] || `Can\'t find entry for ${key}`;
+    }
+  }
 
-
-const x = [
-    [1, 2, 3, 4, 5], // minimum value of row is 1
-    [5, 6, 7, 8, 9], // minimum value of row is 5
-    [20, 21, 34, 56, 100] // minimum value of row is 20
-];
